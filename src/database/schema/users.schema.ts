74c92d82f2e4b0461b85edb651cc4c53f0806b1a
wii <lomugdanglf.19@gmail.com>
2026-09-21 10:13:28 +0800

    schema

--- src/database/schema/users.schema.ts
+++ src/database/schema/users.schema.ts
@@ -1,9 +1,11 @@
+import { pgTable, uuid, varchar, boolean, timestamp } from 'drizzle-orm/pg-core';
 
-import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
-
-export const user = pgTable('users', {
+export const users = pgTable('users', {
   id: uuid('id').primaryKey().defaultRandom(),
-  name: varchar('name', { length: 50 }).notNull(),
+  username: varchar('username', { length: 50 }),
   email: varchar('email', { length: 50 }).notNull(),
-  hash_password: varchar('name', { length: 20 }).notNull(),
-})
+  hash_password: varchar('hash_password', { length: 255 }).notNull(),
+  is_active: boolean('is_active').default(true),
+  created_at: timestamp('created_at').defaultNow(),
+  updated_at: timestamp('updated_at').defaultNow(),
+});
